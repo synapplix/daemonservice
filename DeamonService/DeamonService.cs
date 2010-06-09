40664443df77b3b11ac1dfea9d8240b465d84464
mstipanov <mstipanov@98e7ce3d-861c-4f94-b9c0-8b9ceaf863e8>
2010-06-09 11:45:20 +0000

    version 1.3

--- DeamonService/DeamonService.cs
+++ DeamonService/DeamonService.cs
@@ -15,6 +15,8 @@ namespace DeamonService
     public partial class DeamonService : ServiceBase
     {
         private System.Diagnostics.Process runningProcess;
+        private string runningProcessOutput;
+        private string runningProcessError;
 
         public DeamonService()
         {
@@ -48,12 +50,49 @@ namespace DeamonService
 
         private void ExecuteCommand()
         {
-            String startupScript = getStringSetting("StartupScript");
-            String startupScriptArguments = getStringSetting("StartupScript.Arguments");
-            runningProcess = System.Diagnostics.Process.Start(startupScript, startupScriptArguments);
-            runningProcess.WaitForExit();
-            if (runningProcess.ExitCode != 0)
-                throw new Exception("Error while executing: " + startupScript);
+            try
+            {
+                String startupScript = getStringSetting("StartupScript");
+                String startupScriptArguments = getStringSetting("StartupScript.Arguments");
+                this.EventLog.WriteEntry("Starting process: " + startupScript + " " + startupScriptArguments, EventLogEntryType.Information);
+                Process runningProcess = new Process();
+                // Redirect the output stream of the child process.
+                runningProcess.StartInfo.UseShellExecute = false;
+                runningProcess.StartInfo.RedirectStandardOutput = true;
+                runningProcess.StartInfo.RedirectStandardError = true;
+                runningProcess.StartInfo.FileName = startupScript;
+                runningProcess.StartInfo.Arguments = startupScriptArguments;
+                runningProcess.Start(); 
+
+                runningProcessOutput = runningProcess.StandardOutput.ReadToEnd();
+                runningProcessError = runningProcess.StandardError.ReadToEnd();
+                
+                runningProcess.WaitForExit();
+                if (runningProcess.ExitCode != 0)
+                {
+                    throw new Exception("Error while executing: " + startupScript + "\r\nProcess exited with exit code: " + runningProcess.ExitCode);
+                }
+            }
+            catch (Exception e) {
+                //this.EventLog.WriteEntry(runningProcess.StandardOutput.ReadToEnd(), EventLogEntryType.Error);
+                this.EventLog.WriteEntry(e.Message + ProcessOutput, EventLogEntryType.Error);
+                throw (e);
+            }
+
+        }
+
+        private string ProcessOutput {
+            get {
+                string output = "\r\nOutput:";
+                if (!string.IsNullOrEmpty(runningProcessOutput))
+                    output += "\r\n"+runningProcessOutput;
+                
+                output += "\r\nError:";
+                if (!string.IsNullOrEmpty(runningProcessError))
+                    output += "\r\n" + runningProcessError;
+
+                return output;
+            }
         }
 
         protected override void OnStop()
@@ -76,6 +115,8 @@ namespace DeamonService
             }
             finally {
                 runningProcess = null;
+                runningProcessOutput = null;
+                runningProcessError = null;
             }
         }
 
---
+++ DeamonService/README.txt
@@ -0,0 +1,15 @@
+﻿Release notes:
+v1.3
+ - Release date 2010-06-09
+ - If error happens on startup the standard output and standard error 
+   are displayed in the application event log
+
+v1.2
+ - Release date 2010-06-07
+ - Added Working directory support
+ - Added kill process tree support
+
+v1.1
+ - Release date 2010-05-27
+ - Added script parameters support
+ - Added kill process support
